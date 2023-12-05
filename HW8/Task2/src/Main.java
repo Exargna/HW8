44bee38def63968ee7c1c867b7ea9cb46f2c3755
Exargna <fox7games@mail.ru>
2023-12-05 18:56:16 +0600

    Реализовал все функции, но не знаю, как вывести все данные массива отдельно от людей с весом, ибо если я оставляю sout со всеми данными, то результаты превращаются в кашу

--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //todo код программы писать тут
+        double[] weight = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        double sum = 0;
+        for (int i = 0; i < weight.length; i++) {
+            weight[i] = new Random().nextDouble(40, 100);
+            sum += weight[i];
+
+            if (weight[i] <= 80.9 && weight[i] >= 60) {
+                System.out.println("Люди с весом в диапозоне 80 - 60 кг: " + weight[i]);
+            }
+
+        }
+        System.out.println("\n\nСредний вес всех людей: " + sum / 10);
+
     }
 }
